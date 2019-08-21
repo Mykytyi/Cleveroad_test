68ebddd8c052998cf2de3fdcae119557323da91a
Nikita <cherevaty.nik@gmail.com>
2019-08-21 20:20:21 +0300

    Updates

---
+++ precache-manifest.4d7507fc419f0540e5da468364ffdac5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb00a443069d59ae1a07f833b82f6b8",
+    "url": "/Cleveroad_test_task/index.html"
+  },
+  {
+    "revision": "cec3a070e14089bb66fc",
+    "url": "/Cleveroad_test_task/static/css/main.95ac2473.chunk.css"
+  },
+  {
+    "revision": "a47e458f1e764a464812",
+    "url": "/Cleveroad_test_task/static/js/2.b15025c0.chunk.js"
+  },
+  {
+    "revision": "cec3a070e14089bb66fc",
+    "url": "/Cleveroad_test_task/static/js/main.3962a256.chunk.js"
+  },
+  {
+    "revision": "c89feffd377a3e3c0051",
+    "url": "/Cleveroad_test_task/static/js/runtime~main.ab1d0ad5.js"
+  },
+  {
+    "revision": "50ae75fde984eab67d7fefc72d533691",
+    "url": "/Cleveroad_test_task/static/media/astr.50ae75fd.jpg"
+  }
+]);
